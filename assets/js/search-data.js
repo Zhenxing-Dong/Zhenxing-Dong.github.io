01317ec5464f9c23f0e42ee29288331d9295d513
Zhenxing-Dong <66579268+Zhenxing-Dong@users.noreply.github.com>
2026-02-26 09:07:34 +0000

    Deploying to gh-pages from @ Zhenxing-Dong/Zhenxing-Dong.github.io@63422bb8d8df4c3e9434b54407ac839d778a7aaf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
